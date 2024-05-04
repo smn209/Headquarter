98f10c0531927b0fc203d9827addb169f0440bf6
Jon <jon.r+gh@live.com>
2024-05-04 13:59:36 +0100

    Added option to supply version directly as int

--- code/client/command.c
+++ code/client/command.c
@@ -216,8 +216,14 @@ void parse_command_args(int argc, char **argv)
     }
 
     if (!options.file_game_version[0]) {
+        // No version argument; use hard coded one
         options.game_version = GUILD_WARS_VERSION;
+    }
+    else if (atoi(options.file_game_version) > 0) {
+        // Version argument is numeric
+        options.game_version = (uint32_t)atoi(options.file_game_version);
     } else {
+        // Version argument may be a location on disk of a Gw.build file
         FILE *file;
         if ((file = fopen(options.file_game_version, "rb")) == NULL) {
             log_error("Failed to open '%s', err: %d", options.file_game_version, errno);
